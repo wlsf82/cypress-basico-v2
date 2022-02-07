70db0f52864ffa1df7278c460472cdc0673d00f2
Walmyr <wlsf82@gmail.com>
2022-02-07 01:27:59 +0100

    Create cypress course v2

---
+++ package.json
@@ -0,0 +1,23 @@
+{
+  "name": "curso-cypress-v2",
+  "version": "1.0.0",
+  "description": "Repositório da versão 2 do curso básico de Cypress da Escola Talking About Testing",
+  "main": "src/index.html",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/wlsf82/cypress-basico-v2.git"
+  },
+  "keywords": [
+    "Testing",
+    "Cypress.io",
+    "Talking About Testing",
+    "Cypress Básico",
+    "Escola TAT"
+  ],
+  "author": "Walmyr Filho <wlsf82@gmail.com> (https://walmyr.dev/)",
+  "license": "MIT",
+  "homepage": "https://github.com/wlsf82/cypress-basico-v2#readme"
+}
---
+++ src/style.css
@@ -0,0 +1,188 @@
+* {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+}
+
+#title {
+    color: #fffffe;
+    text-align: center;
+    margin-top: 20px;
+    margin-bottom: 20px;
+}
+
+#subtitle {
+    color: #fffffe;
+    text-align: center;
+}
+
+fieldset {
+    border: 0;
+}
+
+body {
+    background-color: #181818;
+    font-size: 1em;
+    color: #272343;
+    justify-content: center;
+    margin: auto;
+    display: grid;
+}
+
+input, select, textarea, button {
+    border-radius: 5px;
+}
+
+.field {
+    margin-bottom: 1em;
+
+}
+
+.field label {
+    margin-bottom: 0.2em;
+    color: #272343;
+    display: block;
+}
+
+fieldset.group .field {
+    float: left;
+    margin-right: 1em;
+}
+
+.field input[type="text"], .field input[type="email"], .field input[type="number"] {
+    width: 160px;
+}
+
+.field input[type="text"], .field input[type="email"], .field input[type="number"], .field select, .field textarea {
+    padding: 0.2em;
+    border: 1px solid #272343;
+    box-shadow: 2px 2px 2px #181818;
+    display: block;
+}
+
+textarea {
+    width: 100%;
+    height: 100px;
+}
+
+.field select option {
+    padding-right: 1em;
+}
+
+.field input:focus, .field select:focus, .field textarea:focus {
+    background-color: #fffffe;
+}
+
+.button {
+    font-size: 1.2em;
+    background: #4fc4cf;
+    border: #181818 solid 2px;
+    color: #181818;
+    padding: 0.8em 1.2em;
+    font-weight: bold;
+    margin: auto;
+    display: block;
+    margin-top: 20px;
+}
+
+.button:hover {
+    background: #181818;
+    border: #4fc4cf solid 2px;
+    color: #4fc4cf;
+}
+
+#check {
+    display: inline-block;
+}
+
+#support-type label, #check label {
+    margin-bottom: 0.4em;
+}
+
+#check label {
+    display: inline-block;
+    margin-right: 1em;
+}
+
+#white-background {
+    background:#fffffe;
+    display: inline-block;
+    padding: 20px;
+    border-radius: 10px;
+}
+
+input[type="radio"] {
+    margin-right: 5px;
+}
+
+.success {
+    display: none;
+    background-color: #4fc4cf;
+    color: #181818;
+    padding: 8px 10px;
+    border-radius: 5px;
+    margin-bottom: 12px;
+}
+
+.error {
+    display: none;
+    background-color: #fbdd74;
+    color: #181818;
+    padding: 8px 10px;
+    border-radius: 5px;
+    margin-bottom: 12px;
+}
+
+.success, .error {
+    text-align: center;
+}
+
+.required-mark {
+    color: #2e2e2e;
+    font-size: 0.9em;
+}
+
+.phone-label-span {
+    display: none;
+}
+
+#cat {
+    display: none;
+}
+
+#privacy {
+    margin-top: 20px;
+    text-align: center;
+}
+
+#privacy a {
+    color: #4fc4cf;
+    font-weight: bold;
+}
+
+#privacy a:hover {
+    color: #181818;
+}
+
+.privacy h2 {
+    text-align: center;
+}
+
+.privacy p {
+    margin-bottom: 0.2em;
+}
+
+@media (max-width: 411px) {
+    #white-background {
+        border-radius: 0;
+    }
+
+    .error, .success {
+        margin-left: 10px;
+        margin-right: 10px;
+    }
+
+    .field input[type="text"], .field input[type="email"], .field input[type="number"] {
+        width: 330px;
+    }
+}
