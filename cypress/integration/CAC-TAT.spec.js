f9d0c9e86403f9b41ec4d4373259bb901ade92fb
Walmyr <wlsf82@gmail.com>
2022-03-02 16:57:06 +0100

    Demo the use of .each and cy.wrap
    Besides the use of the .should('be.checked') assertion.

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -106,4 +106,13 @@ describe('Central de Atendimento ao Cliente TAT', function() {
       .check()
       .should('have.value', 'feedback')
   })
+
+  it('marca cada tipo de atendimento', function() {
+    cy.get('input[type="radio"]')
+      .should('have.length', 3)
+      .each(function($radio) {
+        cy.wrap($radio).check()
+        cy.wrap($radio).should('be.checked')
+      })
+  })
 })
