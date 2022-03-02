e30d29e02b07c910a7a996979da9ac380ab84ff8
Walmyr <wlsf82@gmail.com>
2022-03-02 16:27:41 +0100

    Demo the use of select by text

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -82,4 +82,10 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
     cy.get('.success').should('be.visible')
   })
+
+  it('seleciona um produto (YouTube) por seu texto', function() {
+    cy.get('#product')
+      .select('YouTube')
+      .should('have.value', 'youtube')
+  })
 })
