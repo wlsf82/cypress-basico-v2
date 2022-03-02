696d2d075e2c9588fc1c1b4773cb744d6bddb553
Walmyr <wlsf82@gmail.com>
2022-03-02 16:30:58 +0100

    Demo the use of .select by value

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -88,4 +88,10 @@ describe('Central de Atendimento ao Cliente TAT', function() {
       .select('YouTube')
       .should('have.value', 'youtube')
   })
+
+  it('seleciona um produto (Mentoria) por seu valor (value)', function() {
+    cy.get('#product')
+      .select('mentoria')
+      .should('have.value', 'mentoria')
+  })
 })
