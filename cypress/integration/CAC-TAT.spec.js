26c375fd35a58b9f8c3597244ff7fcb9d8a49ba3
Walmyr <wlsf82@gmail.com>
2022-03-02 17:33:38 +0100

    Demo file upload simulation drag-n-drop

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -133,4 +133,13 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         expect($input[0].files[0].name).to.equal('example.json')
       })
   })
+
+  it('seleciona um arquivo simulando um drag-and-drop', function() {
+    cy.get('input[type="file"]')
+      .should('not.have.value')
+      .selectFile('./cypress/fixtures/example.json', { action: 'drag-drop' })
+      .should(function($input) {
+        expect($input[0].files[0].name).to.equal('example.json')
+      })
+  })
 })
