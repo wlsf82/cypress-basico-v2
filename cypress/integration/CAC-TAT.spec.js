960ed657564bddff35d063dbf1c85aa3f5ff9626
Walmyr <wlsf82@gmail.com>
2022-03-02 17:13:40 +0100

    Use check instead of click for checking a checkbox
    This improves the test semantics and makes it more robust.

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -41,7 +41,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     cy.get('#firstName').type('Walmyr')
     cy.get('#lastName').type('Filho')
     cy.get('#email').type('walmyr@exemplo.com')
-    cy.get('#phone-checkbox').click()
+    cy.get('#phone-checkbox').check()
     cy.get('#open-text-area').type('Teste')
     cy.contains('button', 'Enviar').click()
 
