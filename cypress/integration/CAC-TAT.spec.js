7b8733e60657d0e4f789d16b52a6b24721c2f16b
Walmyr <wlsf82@gmail.com>
2022-03-02 17:28:57 +0100

    Demo the use of .selectFile

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -124,4 +124,13 @@ describe('Central de Atendimento ao Cliente TAT', function() {
       .uncheck()
       .should('not.be.checked')
   })
+
+  it('seleciona um arquivo da pasta fixtures', function() {
+    cy.get('input[type="file"]')
+      .should('not.have.value')
+      .selectFile('./cypress/fixtures/example.json')
+      .should(function($input) {
+        expect($input[0].files[0].name).to.equal('example.json')
+      })
+  })
 })
