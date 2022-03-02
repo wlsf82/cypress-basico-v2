67a80dbfae53f256eb002dac1dc6eff0657df11c
Walmyr <wlsf82@gmail.com>
2022-03-02 14:39:55 +0100

    Type faster overwriting type's delay

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -10,10 +10,12 @@ describe('Central de Atendimento ao Cliente TAT', function() {
   })
 
   it('preenche os campos obrigatórios e envia o formulário', function() {
+    const longText = 'Teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste.'
+
     cy.get('#firstName').type('Walmyr')
     cy.get('#lastName').type('Filho')
     cy.get('#email').type('walmyr@exemplo.com')
-    cy.get('#open-text-area').type('Teste')
+    cy.get('#open-text-area').type(longText, { delay: 0 })
     cy.get('button[type="submit"]').click()
 
     cy.get('.success').should('be.visible')
