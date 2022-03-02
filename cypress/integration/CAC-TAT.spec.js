2e01278accef6ae9fab47fa165df222fa156f043
Walmyr <wlsf82@gmail.com>
2022-03-02 14:44:53 +0100

    Add test for malformed email

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -20,4 +20,14 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
     cy.get('.success').should('be.visible')
   })
+
+  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function() {
+    cy.get('#firstName').type('Walmyr')
+    cy.get('#lastName').type('Filho')
+    cy.get('#email').type('walmyr@exemplo,com')
+    cy.get('#open-text-area').type('Teste')
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.error').should('be.visible')
+  })
 })
