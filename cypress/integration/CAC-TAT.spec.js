0623e372ae3fa67ae5503629796e77c717e4f8f3
Walmyr <wlsf82@gmail.com>
2022-03-02 15:24:45 +0100

    Demo the use of cy.contains

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -16,7 +16,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     cy.get('#lastName').type('Filho')
     cy.get('#email').type('walmyr@exemplo.com')
     cy.get('#open-text-area').type(longText, { delay: 0 })
-    cy.get('button[type="submit"]').click()
+    cy.contains('button', 'Enviar').click()
 
     cy.get('.success').should('be.visible')
   })
@@ -26,7 +26,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     cy.get('#lastName').type('Filho')
     cy.get('#email').type('walmyr@exemplo,com')
     cy.get('#open-text-area').type('Teste')
-    cy.get('button[type="submit"]').click()
+    cy.contains('button', 'Enviar').click()
 
     cy.get('.error').should('be.visible')
   })
@@ -43,7 +43,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     cy.get('#email').type('walmyr@exemplo.com')
     cy.get('#phone-checkbox').click()
     cy.get('#open-text-area').type('Teste')
-    cy.get('button[type="submit"]').click()
+    cy.contains('button', 'Enviar').click()
 
     cy.get('.error').should('be.visible')
   })
@@ -72,7 +72,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
   })
 
   it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function() {
-    cy.get('button[type="submit"]').click()
+    cy.contains('button', 'Enviar').click()
 
     cy.get('.error').should('be.visible')
   })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,5 +3,5 @@ Cypress.Commands.add('fillMandatoryFieldsAndSubmit', function() {
   cy.get('#lastName').type('Filho')
   cy.get('#email').type('walmyr@exemplo.com')
   cy.get('#open-text-area').type('Teste')
-  cy.get('button[type="submit"]').click()
+  cy.contains('button', 'Enviar').click()
 })
