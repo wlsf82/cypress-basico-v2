b6d0cf1115a8fea8b94fd5cd7525eab60f3fa11d
Walmyr <wlsf82@gmail.com>
2022-03-02 17:55:48 +0100

    Demo the use of .invoke(removeAttr, ...)

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -155,4 +155,12 @@ describe('Central de Atendimento ao Cliente TAT', function() {
   it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function() {
     cy.get('#privacy a').should('have.attr', 'target', '_blank')
   })
+
+  it('acessa a página da política de privacidade removendo o target e então clicanco no link', function() {
+    cy.get('#privacy a')
+      .invoke('removeAttr', 'target')
+      .click()
+
+    cy.contains('Talking About Testing').should('be.visible')
+  })
 })
