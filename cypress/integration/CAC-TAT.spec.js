8ee62df9b1f9451c2d9c5a1aa072f2e0beac9612
Walmyr <wlsf82@gmail.com>
2022-03-02 15:17:03 +0100

    Add test to demo a custom cmd

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -76,4 +76,10 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
     cy.get('.error').should('be.visible')
   })
+
+  it('envia o formuário com sucesso usando um comando customizado', function() {
+    cy.fillMandatoryFieldsAndSubmit()
+
+    cy.get('.success').should('be.visible')
+  })
 })
