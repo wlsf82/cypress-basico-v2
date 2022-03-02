03debf5a0e99b802104f6c331aef8c5b7a9fd4af
Walmyr <wlsf82@gmail.com>
2022-03-02 14:56:22 +0100

    Test when phone becomes mandatory

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -36,4 +36,15 @@ describe('Central de Atendimento ao Cliente TAT', function() {
       .type('abcdefghij')
       .should('have.value', '')
   })
+
+  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function() {
+    cy.get('#firstName').type('Walmyr')
+    cy.get('#lastName').type('Filho')
+    cy.get('#email').type('walmyr@exemplo.com')
+    cy.get('#phone-checkbox').click()
+    cy.get('#open-text-area').type('Teste')
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.error').should('be.visible')
+  })
 })
