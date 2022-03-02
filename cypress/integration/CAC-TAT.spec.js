cd66393ea2edb2ab1d697228feae1b67e6a32400
Walmyr <wlsf82@gmail.com>
2022-03-02 17:50:19 +0100

    Demo the use of .should(have.attr, ...)

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -151,4 +151,8 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         expect($input[0].files[0].name).to.equal('example.json')
       })
   })
+
+  it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function() {
+    cy.get('#privacy a').should('have.attr', 'target', '_blank')
+  })
 })
