afdb8f8ff9f3827ce95b1d840f889c2310e267c6
Walmyr <wlsf82@gmail.com>
2022-03-03 15:26:00 +0100

    Demo the use of invoke val and ._repeat

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -214,4 +214,12 @@ describe('Central de Atendimento ao Cliente TAT', function() {
       .invoke('hide')
       .should('not.be.visible')
   })
+
+  it('preenche a area de texto usando o comando invoke', function() {
+    const longText = Cypress._.repeat('0123456789', 20)
+
+    cy.get('#open-text-area')
+      .invoke('val', longText)
+      .should('have.value', longText)
+  })
 })
