213fed442cee292b1906f1d76ded73c9d80f89a1
Adriano <adrianoaluizi@hotmail.com>
2022-06-30 16:24:05 -0300

    Primeiro teste

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,6 +1,9 @@
-// CAC-TAT.spec.js created with Cypress
-//
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
+/// <reference types="Cypress" />
+
+describe('Central de Atendimento ao Cliente TAT', function() {
+    it('verifica o título da aplicação', function() {
+        cy.visit('./src/index.html')
+
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+  })
