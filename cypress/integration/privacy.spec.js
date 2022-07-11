70a34d66e8abd343d637fbee6f3ae5540ccdd171
marcos Paulo <marcos.sbfj01d@gmail.com>
2022-07-11 13:56:13 -0300

    fix privacity test

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,3 +1,3 @@
-it("testa a página da política de privavidade de forma independente", function () {
-    cy.visit('.src/privacy.html')
+it("testa a página da política de privacidade de forma independente", function () {
+    cy.visit('./src/privacy.html')
   });
