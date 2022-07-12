01f596d8cdf53bdc1e1dfa0529c5670c6c47b342
AndreHammel <hammel.engineer@gmail.com>
2022-07-12 10:58:16 -0300

    setup feito

---
+++ cypress/integration/CAC-TAT.spec.js
@@ -0,0 +1,6 @@
+// CAC-TAT.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
