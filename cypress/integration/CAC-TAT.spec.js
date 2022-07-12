eb2f3b0b61094f1d373653f6d4a31aee186afc3f
AndreHammel <hammel.engineer@gmail.com>
2022-07-12 11:26:36 -0300

    primeiro teste usando title - should- be.equal

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,6 +1,7 @@
-// CAC-TAT.spec.js created with Cypress
-//
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
+
+describe('Central de Atendimento ao Cliente TAT', () => {
+  it('verifica o título de aplicação', () => {
+    cy.visit('./src/index.html')
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+  })
+})
