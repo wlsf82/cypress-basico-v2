dd3562c3b6c02cfdf06883bd7c5cfb907d8d1b4d
wellington197 <fcowellcosta@gmail.com>
2022-07-20 16:25:26 -0300

    alterando arquivo config.js v1

--- cypress.config.js
+++ cypress.config.js
@@ -4,15 +4,14 @@ module.exports = defineConfig({
 
   viewportHeight: 880,
   viewportWidth: 1280,
-
   
 
 
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
-   /* setupNodeEvents(on, config) {
-      return require (on, config)
-    },*/
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
   },
 })
