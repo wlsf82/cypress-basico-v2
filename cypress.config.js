34f2cd53e09c7e80ecd029fd592c064424e98654
wellington197 <fcowellcosta@gmail.com>
2022-07-20 15:32:01 -0300

    Testes da aplicação no formato Mobile. Feito a criação do arquivo de CI para integração contínua

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,11 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  
   viewportHeight: 880,
   viewportWidth: 1280,
+
+
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
