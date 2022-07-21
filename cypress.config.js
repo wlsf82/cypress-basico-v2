af73d0ca3c2d5a80d40d39b7722f5e5d8461cb1d
wellington197 <fcowellcosta@gmail.com>
2022-07-21 16:16:54 -0300

    Atualização do CI v3

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,4 @@
-/*const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
 
@@ -15,21 +15,3 @@ module.exports = defineConfig({
     },
   },
 })
-*/
-
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
-  chromeWebSecurity: false,
-  defaultCommandTimeout: 15000,
-  retries: 2,
-  video: false,
-  e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require("./cypress/plugins/index.js")(on, config);
-    },
-    baseUrl: "http://localhost:8000/",
-  },
-});
