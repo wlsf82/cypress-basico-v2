2f5bd26d505dd0b41f7feaef09eb6b696847e9d6
wellington197 <fcowellcosta@gmail.com>
2022-07-21 16:49:50 -0300

    Quebrando arquivo de propósito

--- cypress.config.js
+++ cypress.config.js
@@ -11,7 +11,6 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      //return require('./cypress/plugins/index.js')(on, config)
     },
   },
 })
