0009cb2e4c8fd7682e8dce234159e4edbc567f03
wellington197 <fcowellcosta@gmail.com>
2022-07-21 16:02:54 -0300

    Atualização do CI v2

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require('cypress')
+/*const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
 
@@ -11,7 +11,25 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require (on, config)
+      return require('./cypress/plugins/index.js')(on, config)
     },
   },
 })
+*/
+
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 15000,
+  retries: 2,
+  video: false,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require("./cypress/plugins/index.js")(on, config);
+    },
+    baseUrl: "http://localhost:8000/",
+  },
+});
--- cypress/plugins/.index.js
+++ cypress/plugins/.index.js
@@ -19,4 +19,12 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+
+  on('before:browser:launch', (browser = {}, launchOptions) => {
+    if (browser.name === 'chrome') {
+      launchOptions.args.push('--disable-web-security');
+    }
+
+    return launchOptions;
+  });
 }
