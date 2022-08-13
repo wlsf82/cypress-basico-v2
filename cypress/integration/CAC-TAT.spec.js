8f28ea5818afa1fec66c141e23a1a65685a8d7f5
AnaStadelhofer <87942437+AnaStadelhofer@users.noreply.github.com>
2022-08-13 13:42:04 -0300

    Second extra task

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -4,10 +4,10 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     beforeEach(() => {
         cy.visit('./src/index.html')
     })
-    it('verifica o título da aplicação', function() {
+    it('Verifica o título da aplicação', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
-    it('preenche os campos obrigatórios e envia o formulário', function() {
+    it('Preenche os campos obrigatórios e envia o formulário', function() {
         cy.get('#firstName').type('Ana')
         cy.get('#lastName').type('Carolina Stadelhofer')
         cy.get('#email').type('ana.stadelhofer@cypress.com')
@@ -19,5 +19,14 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     it('Alterado o delay da digitação', function() {
         const longText = 'Texto longo, Texto longo, Texto longo, Texto longo, Texto longo, Texto longo, Texto longo, Texto longo, Texto longo, Texto longo, Texto longo, Texto longo, Texto longo, Texto longo, Texto longo, Texto longo, Texto longo, Texto longo, Texto longo, Texto longo, Texto longo, Texto longo, Texto longo'
         cy.get('#open-text-area').type(longText, { delay: 0 })
+    })
+
+    it('Exibe mensagem de erro ao submeter o formulário com um email com formatação inválid', function() {
+        cy.get('#firstName').type('Ana')
+        cy.get('#lastName').type('Carolina Stadelhofer')
+        cy.get('#email').type('ana.stadelhofer')
+        cy.get('#open-text-area').type('Gostaria de fazer um pedido de atendimento, porém estou com dificuldades de realizar o contato.')
+        cy.get('.button').click()
+        cy.get('.error').should('be.visible')
     })
   })
