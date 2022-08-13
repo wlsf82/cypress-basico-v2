7c5f6946c363969b1d486423d9295e6ce7fe3775
AnaStadelhofer <87942437+AnaStadelhofer@users.noreply.github.com>
2022-08-13 13:06:35 -0300

    First task

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,9 +1,18 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
+    beforeEach(() => {
         cy.visit('./src/index.html')
+    })
+    it('verifica o título da aplicação', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
-
+    })
+    it('preenche os campos obrigatórios e envia o formulário', function() {
+        cy.get('#firstName').type('Ana')
+        cy.get('#lastName').type('Carolina Stadelhofer')
+        cy.get('#email').type('ana.stadelhofer@cypress.com')
+        cy.get('#open-text-area').type('Gostaria de fazer um pedido de atendimento, porém estou com dificuldades de realizar o contato.')
+        cy.get('.button').click()
+        cy.get('.success').should('be.visible')
     })
   })
