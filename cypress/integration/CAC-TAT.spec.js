6bf3d1c9f182bdd97763792ebde9e188979cf6a6
AnaStadelhofer <87942437+AnaStadelhofer@users.noreply.github.com>
2022-08-14 14:08:04 -0300

    Link page - invoke extra

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -142,7 +142,12 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         })
     })
 
-    it.only('Verificar que a política de privacidade abre em outra aba sem a necessidade de um clique', function() {
+    it('Verificar que a política de privacidade abre em outra aba sem a necessidade de um clique', function() {
         cy.get('#privacy a').should('have.attr', 'target', '_blank')
+    })
+
+    it.only('acessa a página da política de privacidade removendo o target e então clicanco no link', function() {
+        cy.get('#privacy a').invoke('removeAttr', 'target').click()
+        cy.contains('Talking About Testing').should('be.visible')
     })
   })
