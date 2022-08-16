38ad1d4fc4cdbdab7e913f079eea9dc7f4056cdc
AnaStadelhofer <87942437+AnaStadelhofer@users.noreply.github.com>
2022-08-16 18:16:26 -0300

    request http

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -198,9 +198,18 @@ describe('Central de Atendimento ao Cliente TAT', function() {
           .should('not.be.visible')
       })
 
-      it.only('preenche a area de texto usando o comando invoke', function() {
+      it('preenche a area de texto usando o comando invoke', function() {
         const longText = Cypress._.repeat('1234', 20)
 
         cy.get('#open-text-area').invoke('val', longText).should('have.value', longText)
+      })
+
+      it.only("Fazer uma requisição via HTTP", function() {
+        cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html').should(function(response) {
+            const { status, statusText, body } = response 
+            expect(status).to.equal(200)
+            expect(statusText).to.equal('OK')
+            expect(body).to.include('CAC TAT')
+        })
       })
 })
