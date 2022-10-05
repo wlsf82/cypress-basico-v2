53a598e545bb9d34038581f2c655b9bd285e036c
Suelen Francisco Marques <59808425+Suelen-Francisco-Marqes@users.noreply.github.com>
2022-10-04 21:18:53 -0300

    conhecendo novo comandos em cyprees

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -189,5 +189,29 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.contains('Talking About Testing').should('be.visible')
     })
 
+    it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', () => {
+        cy.get('.success')
+          .should('not.be.visible')
+          .invoke('show')
+          .should('be.visible')
+          .and('contain', 'Mensagem enviada com sucesso.')
+          .invoke('hide')
+          .should('not.be.visible')
+        cy.get('.error')
+          .should('not.be.visible')
+          .invoke('show')
+          .should('be.visible')
+          .and('contain', 'Valide os campos obrigatórios!')
+          .invoke('hide')
+          .should('not.be.visible')
+    })
+
+      it.only('preeche a area de texto usando comando invoke', () => {
+        const longText = Cypress._.repeat('0123456789', 20)
+
+        cy.get('#open-text-area')
+          .invoke('val', longText)
+          .should('have.value', longText)
+      })
 })
 
--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,5 +1,8 @@
-it.only('testa a pagina de politica de privacidade de forma independente', function(){
-    cy.visit('./src/privacy.html')
+Cypress._.times(3, function() {
+    it('testa a pagina de politica de privacidade de forma independente', function(){
+        cy.visit('./src/privacy.html')
 
-    cy.contains('Talking About Testing').should('be.visible')
+        cy.contains('Talking About Testing').should('be.visible')
+    })
+
 })
