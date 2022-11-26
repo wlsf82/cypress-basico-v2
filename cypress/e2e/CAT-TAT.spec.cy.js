e35ee931fb9309890d480e801595e88bf8a8e8b2
MateusAra <mateus19araujo@hotmail.com>
2022-11-26 09:47:33 -0300

    Aula 02 - Interagindo com elementos da pagina

--- cypress/e2e/CAT-TAT.spec.cy.js
+++ cypress/e2e/CAT-TAT.spec.cy.js
@@ -1,7 +1,18 @@
 /// <reference types="Cypress"/>
 
 describe('Central de Atendimento ao cliente TAT', function(){
-    it('verfica o titulo da aplicação', function(){
-        
+    beforeEach(function(){
+        cy.visit('../../src/index.html')
     })
-})
+    it('verfica o titulo da aplicação', function(){
+        cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
+    });
+    it.only('preenche os campos obrigatórios e envia formulário',function(){
+        cy.get('#firstName').type('Mateus')
+        cy.get('#lastName').type('Araujo')
+        cy.get('#email').type('mateus@gmail.com')
+        cy.get('#open-text-area').type('#endRegion')
+        cy.get('button[type="submit"]').click()
+        cy.get('.success').should('be.visible')
+    });
+});
