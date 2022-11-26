1fa2b896e23de476a8a2cd13b2c376e1cefed34d
MateusAra <mateus19araujo@hotmail.com>
2022-11-26 10:40:19 -0300

    Protegendo dados sensiveis

--- cypress/e2e/CAT-TAT.spec.cy.js
+++ cypress/e2e/CAT-TAT.spec.cy.js
@@ -11,16 +11,16 @@ describe('Central de Atendimento ao cliente TAT', function(){
         const longText = 'O comando cy.get() passa adiante ao próximo comando o elemento encontrado, e outros comandos, tais como o .type() e click() (dentre outros), também passam adiante o mesmo elemento, possibilitando encadearmos novos comandos, por exemplo, para fazer uma verificação.'
         cy.get('#firstName').type('Mateus')
         cy.get('#lastName').type('Araujo')
-        cy.get('#email').type('mateus@gmail.com')
-        cy.get('#open-text-area').type(longText, { delay: 0 })
+        cy.get('#email').type('mateus@gmail,com', {log: false})
+        cy.get('#open-text-area').type('testestestestestes', {log: false})
         cy.contains('button', 'Enviar').click()
         cy.get('.success').should('be.visible')
     });
     it('exibe mensagem de erro ao submeter formulário com um email com  formatação inválida',function(){
         cy.get('#firstName').type('Mateus')
         cy.get('#lastName').type('Araujo')
-        cy.get('#email').type('mateus@gmail,com')
-        cy.get('#open-text-area').type('testestestestestes')
+        cy.get('#email').type('mateus@gmail,com', {log: false})
+        cy.get('#open-text-area').type('testestestestestes', {log: false})
         cy.contains('button', 'Enviar').click()
         cy.get('.error').should('be.visible')
     });
