2860f44836206eb3cbe3e008e9f35691788d5635
Henrique Silveira <henrique.silveira@tech4h.com.br>
2022-12-07 22:09:52 -0300

    Lesson Three - Done

--- cypress/e2e/lessonThree/testLessonThree.spec.cy.js
+++ cypress/e2e/lessonThree/testLessonThree.spec.cy.js
@@ -3,15 +3,15 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     cy.visit('./src/index.html')
   })
 
-  it('seleciona um produto (YouTube) por seu texto', function () {
+  it('Seleciona um produto (YouTube) por seu texto', function () {
     cy.get('#product').select('YouTube').should('have.value', 'youtube')
   })
 
-  it('seleciona um produto (Mentoria) por seu valor (value)', function () {
+  it('Seleciona um produto (Mentoria) por seu valor (value)', function () {
     cy.get('#product').select('mentoria').should('have.value', 'mentoria')
   })
 
-  it('seleciona um produto (Blog) por seu índice', function () {
+  it('Seleciona um produto (Blog) por seu índice', function () {
     cy.get('#product').select(1).should('have.value', 'blog')
   })
 })
