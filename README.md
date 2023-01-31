5a0367c2d5adbc296f39c53591cd65a591264b1e
amandarazzano <amandagrazzano@gmail.com>
2023-01-31 16:32:32 -0300

    Create cypress project

--- README.md
+++ README.md
@@ -1,29 +1,30 @@
-# Testes automatizados com Cypress - Básico
+# Testes automatizados com Cypress - Básico 2
 
-👋 Seja bem-vindo(a)!
+Projeto com a prática dos exercios do curso de Testes automatizados com Cypress - Básico.
 
-É muito bom tê-lo(a) aqui. Tenho certeza que você vai adorar este curso. ❤️
+## Pré-Requisitos
 
-## O que você vai aprender
+É necessário ter Node.js e npm instalados para executar este projeto.
 
-Durante o curso de testes automatizados com Cypress (básico), você vai aprender:
+## Instalação
 
-- Como configurar um projeto Cypress do zero
-- Como visitar páginas locais e remotas
-- Como lidar com os elementos mais comuns encontrados em aplicações web
-- Como testar _upload_ de arquivos
-- Como realizar as mais diversas verificações de resultados esperados
-- Como criar comandos customizados
-- Como lidar com links que abrem em outra aba do navegador
-- Como rodar testes simulando as dimensões de um dispositivo móvel
-- Como resolver os mesmos problemas de diferentes formas, conhecendo a [API do Cypress](https://docs.cypress.io/api/table-of-contents)
-- Como executar os testes em um _pipeline_ de integração contínua sempre que mudanças ocorrerem no código da aplicação (ou dos testes)
-- Como criar uma documentação mínima para seu projeto de testes automatizados
+Execute npm install (ou npm i para a versão curta) para instalar as dependências do desenvolvedor.
 
-## Vamos começar?
+## Testes 
 
-Vá para a seção [estrutura do curso](./lessons/_course-structure_.md).
+Você pode executar os testes simulando desktop ou mobile viewport.
 
-___
+###Desktop
 
-Este é mais um curso da [**Escola Talking About Testing**](https://udemy.com/user/walmyr).
+Run `npm test` (ou `npm t` para a versão curta) para executar o teste headless no modo desktop viewport.
+
+Ou, run `npm run cy:open` para abrir o Cypress interativo no modo desktop viewport.
+
+###Mobile
+
+Run `npm test:mobile` para executar o teste no modo headless mobile viewport.
+
+Ou, run `npm run cy:open:mobile` para abrir o Cypress no modo interativo mobile viewport.
+
+
+
--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -15,7 +15,7 @@ describe("Central de Atendimento do Cliente TAT", function () {
     cy.get("#lastName").type("Silva");
     cy.get("#email").type("e@mail.com");
     cy.get("#open-text-area").type(longText, { delay: 0 });
-    cy.get('button[type="submit"]').click();
+    cy.contains("button", "Enviar").click();
     cy.get(".success").should("be.visible");
   });
 
@@ -24,7 +24,7 @@ describe("Central de Atendimento do Cliente TAT", function () {
     cy.get("#lastName").type("Silva");
     cy.get("#email").type("e@com");
     cy.get("#open-text-area").type("Entre em contato");
-    cy.get('button[type="submit"]').click();
+    cy.contains("button", "Enviar").click();
     cy.get(".error").should("be.visible");
   });
 
@@ -36,7 +36,7 @@ describe("Central de Atendimento do Cliente TAT", function () {
     cy.get("#firstName").type("Alice");
     cy.get("#lastName").type("Silva");
     cy.get("#email").type("e@test.com.br");
-    cy.get("#phone-checkbox").click();
+    cy.get("#phone-checkbox").check();
     cy.get("#open-text-area").type("Entre em contato");
     cy.get('button[type="submit"]').click();
     cy.get(".error").should("be.visible");
@@ -65,8 +65,84 @@ describe("Central de Atendimento do Cliente TAT", function () {
       .should("have.value", "");
   });
 
-  it.only("exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios", function () {
-    cy.get('button[type="submit"]').click();
-    cy.get('.error').should('be.visible');
+  it("exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios", function () {
+    cy.contains("button", "Enviar").click();
+    cy.get(".error").should("be.visible");
+  });
+
+  it("envia o formuário com sucesso usando um comando customizado", function () {
+    cy.fillMandatoryFieldsAndSubmit();
+    cy.get(".success").should("be.visible");
+  });
+
+  it("seleciona um produto (YouTube) por seu texto", function () {
+    cy.get("#product").select("youtube").should("have.value", "youtube");
+  });
+
+  it("seleciona um produto (Mentoria) por seu valor (value)", function () {
+    cy.get("#product").select("mentoria").should("have.value", "mentoria");
+  });
+
+  it("seleciona um produto (Blog) por seu índice", function () {
+    cy.get("#product").select(1).should("have.value", "blog");
+  });
+
+  it('marca o tipo de atendimento "Feedback"', function () {
+    cy.get('input[value="feedback"]').check().should("have.value", "feedback");
+  });
+
+  it("marca cada tipo de atendimento", function () {
+    cy.get('input[type="radio"]')
+      .should("have.length", 3)
+      .each(function ($radio) {
+        cy.wrap($radio).check();
+        cy.wrap($radio).should("be.checked");
+      });
+  });
+  it("marca ambos checkboxes, depois desmarca o último", function () {
+    cy.get('input[type="checkbox"]')
+      .as("checkboxes")
+      .check()
+      .should("be.checked")
+      .last()
+      .uncheck()
+      .should("not.be.checked");
+  });
+
+  it("seleciona um arquivo da pasta fixtures", function () {
+    cy.get('input[type="file"]')
+      .should("not.have.value")
+      .selectFile("cypress/fixtures/example.json")
+      .should(function ($input) {
+        expect($input[0].files[0].name).to.equal("example.json");
+      });
+  });
+
+  it("seleciona um arquivo simulando um drag-and-drop", function () {
+    cy.get('input[type="file"]')
+      .should("not.have.value")
+      .selectFile("cypress/fixtures/example.json", { action: "drag-drop" })
+      .should(function ($input) {
+        expect($input[0].files[0].name).to.equal("example.json");
+      });
+  });
+
+  it("seleciona um arquivo utilizando uma fixture para a qual foi dada um alias", function () {
+    cy.fixture("example.json").as("sampleFile");
+    cy.get('input[type="file"]')
+      .selectFile("@sampleFile")
+      .should(function ($input) {
+        expect($input[0].files[0].name).to.equal("example.json");
+      });
+  });
+
+  it("verifica que a política de privacidade abre em outra aba sem a necessidade de um clique", function () {
+    cy.get("#privacy a").should("have.attr", "target", "_blank");
+  });
+
+  it("acessa a página da política de privacidade removendo o target e então clicando no link", function () {
+    cy.get('a[href="privacy.html"]').invoke("removeAttr", "target").click();
+
+    cy.contains("Talking About Testing").should("be.visible");
   });
 });
