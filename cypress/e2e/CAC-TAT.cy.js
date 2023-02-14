66d21d394f794e62ca588736ae6359d94f3c28cc
Walmyr <wlsf82@gmail.com>
2023-02-14 02:32:00 +0100

    Remove tests unrelated to business rules

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -70,29 +70,6 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     cy.get('.error').should('not.be.visible')
   })
 
-  it('preenche e limpa os campos nome, sobrenome, email e telefone', function() {
-    cy.get('#firstName')
-      .type('Walmyr')
-      .should('have.value', 'Walmyr')
-      .clear()
-      .should('have.value', '')
-    cy.get('#lastName')
-      .type('Filho')
-      .should('have.value', 'Filho')
-      .clear()
-      .should('have.value', '')
-    cy.get('#email')
-      .type('walmyr@exemplo.com')
-      .should('have.value', 'walmyr@exemplo.com')
-      .clear()
-      .should('have.value', '')
-    cy.get('#phone')
-      .type('1234567890')
-      .should('have.value', '1234567890')
-      .clear()
-      .should('have.value', '')
-  })
-
   it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function() {
     cy.clock()
 
@@ -117,110 +94,8 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     cy.get('.success').should('not.be.visible')
   })
 
-  it('seleciona um produto (YouTube) por seu texto', function() {
-    cy.get('#product')
-      .select('YouTube')
-      .should('have.value', 'youtube')
-  })
-
-  it('seleciona um produto (Mentoria) por seu valor (value)', function() {
-    cy.get('#product')
-      .select('mentoria')
-      .should('have.value', 'mentoria')
-  })
-
-  it('seleciona um produto (Blog) por seu índice', function() {
-    cy.get('#product')
-      .select(1)
-      .should('have.value', 'blog')
-  })
-
-  it('marca o tipo de atendimento "Feedback"', function() {
-    cy.get('input[type="radio"][value="feedback"]')
-      .check()
-      .should('have.value', 'feedback')
-  })
-
-  it('marca cada tipo de atendimento', function() {
-    cy.get('input[type="radio"]')
-      .should('have.length', 3)
-      .each(function($radio) {
-        cy.wrap($radio).check()
-        cy.wrap($radio).should('be.checked')
-      })
-  })
-
-  it('marca ambos checkboxes, depois desmarca o último', function() {
-    cy.get('input[type="checkbox"]')
-      .check()
-      .should('be.checked')
-      .last()
-      .uncheck()
-      .should('not.be.checked')
-  })
-
-  it('seleciona um arquivo da pasta fixtures', function() {
-    cy.get('input[type="file"]')
-      .should('not.have.value')
-      .selectFile('./cypress/fixtures/example.json')
-      .should(function($input) {
-        expect($input[0].files[0].name).to.equal('example.json')
-      })
-  })
-
-  it('seleciona um arquivo simulando um drag-and-drop', function() {
-    cy.get('input[type="file"]')
-      .should('not.have.value')
-      .selectFile('./cypress/fixtures/example.json', { action: 'drag-drop' })
-      .should(function($input) {
-        expect($input[0].files[0].name).to.equal('example.json')
-      })
-  })
-
-  it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', function() {
-    cy.fixture('example.json').as('sampleFile')
-    cy.get('input[type="file"]')
-      .selectFile('@sampleFile')
-      .should(function($input) {
-        expect($input[0].files[0].name).to.equal('example.json')
-      })
-  })
-
   it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function() {
     cy.get('#privacy a').should('have.attr', 'target', '_blank')
-  })
-
-  it('acessa a página da política de privacidade removendo o target e então clicanco no link', function() {
-    cy.get('#privacy a')
-      .invoke('removeAttr', 'target')
-      .click()
-
-    cy.contains('Talking About Testing').should('be.visible')
-  })
-
-  it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', function() {
-    cy.get('.success')
-      .should('not.be.visible')
-      .invoke('show')
-      .should('be.visible')
-      .and('contain', 'Mensagem enviada com sucesso.')
-      .invoke('hide')
-      .should('not.be.visible')
-    cy.get('.error')
-      .should('not.be.visible')
-      .invoke('show')
-      .should('be.visible')
-      .and('contain', 'Valide os campos obrigatórios!')
-      .invoke('hide')
-      .should('not.be.visible')
-  })
-
-  it('preenche a area de texto usando o comando invoke', function() {
-    const longText = Cypress._.repeat('0123456789', 20)
-
-    cy.get('#open-text-area')
-      .invoke('val', longText)
-      .should('have.value', longText)
   })
 
   it('faz uma requisição HTTP', function()  {
@@ -231,15 +106,5 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         expect(statusText).to.equal('OK')
         expect(body).to.include('CAC TAT')
       })
-  })
-
-  it('encontra o gato escondido', function() {
-    cy.get('#cat')
-      .invoke('show')
-      .should('be.visible')
-    cy.get('#title')
-      .invoke('text', 'CAT TAT')
-    cy.get('#subtitle')
-      .invoke('text', 'Eu 💚 gatos!')
   })
 })
