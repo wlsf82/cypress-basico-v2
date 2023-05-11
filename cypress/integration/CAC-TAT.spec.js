851b09abca60d076c41e741f714a5ccd51ee7324
patriciapossarii <patricia.mag.carvalho@gmail.com>
2023-05-11 19:52:17 -0300

    Teste - verifica o título da aplicação'

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,6 +1,10 @@
-// CAC-TAT.spec.js created with Cypress
-//
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
+/// <reference types="Cypress" />
+
+describe('Central de Atendimento ao Cliente TAT', function () {
+    it('verifica o título da aplicação', function () {
+        cy.visit('./src/index.html')
+        .title()
+        .should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+
+})
