44905b46e563cbd36af97e468eebd9de41c241de
patriciapossarii <patricia.mag.carvalho@gmail.com>
2023-05-15 13:47:30 -0300

    Aula02 - Exercicio - Extra2 e Extra3

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -22,8 +22,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     })
 
 
-    it.only('preenche os campos obrigatórios e envia o formulário - EXTRA1 - Texto Longo - delay', function () {
-        //cy.get('input[type="text"]').should('be.visible').type('Olá mundo!').should('have.value', 'Olá mundo!')
+    it('preenche os campos obrigatórios e envia o formulário - EXTRA1 - Texto Longo - delay', function () {
         cy.get('#firstName').should('be.visible').type('Patricia').should('have.value', 'Patricia')
         cy.get('#lastName').should('be.visible').type('Possari').should('have.value', 'Possari')
         cy.get('#email').should('be.visible').type('patricia.possari@teste.com').should('have.value', 'patricia.possari@teste.com')
@@ -31,5 +30,24 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('button[type="submit"]').click()
         cy.get('.success').should('be.visible')
  })
+
+ it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida- EXTRA2 - Formatação email incorreta', function () {
+    cy.get('#firstName').should('be.visible').type('Patricia').should('have.value', 'Patricia')
+    cy.get('#lastName').should('be.visible').type('Possari').should('have.value', 'Possari')
+    cy.get('#email').should('be.visible').type('patricia.possari_teste.com').should('have.value', 'patricia.possari_teste.com')
+    cy.get('#open-text-area').should('be.visible').type('Mussum Ipsum, cacilds vidis litro abertis. Copo furadis é disculpa de bebadis, arcu quam euismod magna.Quem manda na minha terra sou euzis!Suco de cevadiss deixa as pessoas mais interessantis.Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi.', { delay: 0 }).should('have.value', 'Mussum Ipsum, cacilds vidis litro abertis. Copo furadis é disculpa de bebadis, arcu quam euismod magna.Quem manda na minha terra sou euzis!Suco de cevadiss deixa as pessoas mais interessantis.Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi.')
+    cy.get('button[type="submit"]').click()
+    cy.get('.error').should('be.visible')
+})
+
+it.only('campo de telefone, validar que, se um valor não-numérico for digitado, seu valor continuará vazio- EXTRA3', function () {
+    cy.get('#firstName').should('be.visible').type('Patricia').should('have.value', 'Patricia')
+    cy.get('#lastName').should('be.visible').type('Possari').should('have.value', 'Possari')
+    cy.get('#email').should('be.visible').type('patricia.possari_teste.com').should('have.value', 'patricia.possari_teste.com')
+    cy.get('#phone').should('be.visible').type('Possari').should('have.value', '')
+    cy.get('#open-text-area').should('be.visible').type('Mussum Ipsum, cacilds vidis litro abertis. Copo furadis é disculpa de bebadis, arcu quam euismod magna.Quem manda na minha terra sou euzis!Suco de cevadiss deixa as pessoas mais interessantis.Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi.', { delay: 0 }).should('have.value', 'Mussum Ipsum, cacilds vidis litro abertis. Copo furadis é disculpa de bebadis, arcu quam euismod magna.Quem manda na minha terra sou euzis!Suco de cevadiss deixa as pessoas mais interessantis.Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi.')
+    cy.get('button[type="submit"]').click()
+    cy.get('.error').should('be.visible')
+})
    
 })
