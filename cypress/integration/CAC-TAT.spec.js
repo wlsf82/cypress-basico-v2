3e37b8918703a8aeba196f07c47a3349f92aab77
Rafael Almeida <rafael13almeida@gmail.com>
2023-05-15 13:45:17 -0300

    Adicionando primeiro teste

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,6 +1,22 @@
-// CAC-TAT.spec.js created with Cypress
-//
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
+/// <reference types="Cypress" />
+
+describe('Central de Atendimento ao Cliente TAT', function() {
+    
+    beforeEach(() => {
+        cy.visit('./src/index.html');
+    })
+
+    it('verifica o título da aplicação', function() {  
+        cy.title().should('eq', 'Central de Atendimento ao Cliente TAT');    
+    });
+
+    it('Preenche os campos obrigatórios e envia o formulário', () => {
+        cy.get('#firstName').type('Rafael')
+        cy.get('#lastName').type('Almeida')
+        cy.get('#email').type('almeida@mail.com')
+        cy.get('#open-text-area').type('Testando marotamente, fazendo um teste com uma linha maior', {delay: 1})
+        cy.get('button[type="submit"]').click()
+        cy.get('.success > strong').should('be.visible')
+    }) 
+  })
+  
