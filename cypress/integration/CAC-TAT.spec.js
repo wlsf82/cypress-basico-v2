42a747dc181a072d2d0033634aa7215e448f7bf4
patriciapossarii <patricia.mag.carvalho@gmail.com>
2023-05-15 12:08:47 -0300

    Aula02 - Exercicio - Extra1

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -11,7 +11,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         .should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
 
-    it.only('preenche os campos obrigatórios e envia o formulário', function () {
+    it('preenche os campos obrigatórios e envia o formulário', function () {
            //cy.get('input[type="text"]').should('be.visible').type('Olá mundo!').should('have.value', 'Olá mundo!')
            cy.get('#firstName').should('be.visible').type('Patricia').should('have.value', 'Patricia')
            cy.get('#lastName').should('be.visible').type('Possari').should('have.value', 'Possari')
@@ -19,9 +19,17 @@ describe('Central de Atendimento ao Cliente TAT', function () {
            cy.get('#open-text-area').should('be.visible').type('Teste de envio').should('have.value', 'Teste de envio')
            cy.get('button[type="submit"]').click()
            cy.get('.success').should('be.visible')
-
-
-
     })
+
+
+    it.only('preenche os campos obrigatórios e envia o formulário - EXTRA1 - Texto Longo - delay', function () {
+        //cy.get('input[type="text"]').should('be.visible').type('Olá mundo!').should('have.value', 'Olá mundo!')
+        cy.get('#firstName').should('be.visible').type('Patricia').should('have.value', 'Patricia')
+        cy.get('#lastName').should('be.visible').type('Possari').should('have.value', 'Possari')
+        cy.get('#email').should('be.visible').type('patricia.possari@teste.com').should('have.value', 'patricia.possari@teste.com')
+        cy.get('#open-text-area').should('be.visible').type('Mussum Ipsum, cacilds vidis litro abertis. Copo furadis é disculpa de bebadis, arcu quam euismod magna.Quem manda na minha terra sou euzis!Suco de cevadiss deixa as pessoas mais interessantis.Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi.', { delay: 0 }).should('have.value', 'Mussum Ipsum, cacilds vidis litro abertis. Copo furadis é disculpa de bebadis, arcu quam euismod magna.Quem manda na minha terra sou euzis!Suco de cevadiss deixa as pessoas mais interessantis.Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi.')
+        cy.get('button[type="submit"]').click()
+        cy.get('.success').should('be.visible')
+ })
    
 })
