9805b8e68eed8bdb36855e796d0bfc180bdeefb8
Daniel Silva <dfsilva.dxp@gmail.com>
2023-05-16 18:55:25 -0300

    test: este commit irá adicionar o primeiro teste com cypress.

---
+++ cypress/e2e/CAC-TAT.spec.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="Cypress" />
+
+describe("Central de Atendimento ao Cliente TAT", () => {
+  beforeEach(() => {
+    cy.visit("./src/index.html");
+  });
+
+  it("verifica o título da aplicação", () => {
+    cy.title().should("be.equal", "Central de Atendimento ao Cliente TAT");
+  });
+});
