98cbbe00de63ed845b3748c860ea662c9379d69b
Walid Arnous <walidarnous.1@gmail.com>
2023-05-17 19:15:53 -0300

    exercicio exemplo 1

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,10 +1,23 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
+    beforeEach(function() {
+        //Antes de cada teste, abre o navegador
         cy.visit('./src/index.html')
+    })
 
+    it('verifica o título da aplicação', function() {
         cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
+    })
+
+    it.only('preenche os campos obrigatórios e envia o formulário', function(){
+        cy.get('#firstName').type('Walid')
+        cy.get('#lastName').type('Arnous')
+        cy.get('#email').type('walid@teste.com.br')
+        cy.get('#open-text-area').type('Teste Campo')
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.success').should('be.visible')
     })
 })
   
