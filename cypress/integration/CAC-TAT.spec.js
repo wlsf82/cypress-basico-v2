fd37e5404a1a3706750e3b9bf40d14fa1b4db12c
Rafael Almeida <rafael13almeida@gmail.com>
2023-05-17 20:14:44 -0300

    Adicionando testes com input do tipo radio

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -120,6 +120,20 @@ describe('Central de Atendimento ao Cliente TAT', function() {
             .should('have.value', 'blog');
     });
 
+    it('marca o tipo de atendimento "Feedback"', () => {
+        cy.get('input[type="radio"][value="feedback"]')
+            .check('feedback')
+            .should('be.checked', 'feedback');
+    });
+
+    it('marca cada tipo de atendimento', () => {
+        cy.get('input[type="radio"]')
+            .should('have.length', 3)
+            .each(function($radio) {
+                cy.wrap($radio).check()
+                cy.wrap($radio).should('be.checked')
+        }) 
+    });
 })
   
 
