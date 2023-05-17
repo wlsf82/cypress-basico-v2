138fac6ace372c4de770a7e1e578c3d087238cdc
Daniel Silva <dfsilva.dxp@gmail.com>
2023-05-17 19:01:33 -0300

    test: este commit adiciona testes para digitar em campos e clicar em elementos.

--- cypress/e2e/CAC-TAT.spec.cy.js
+++ cypress/e2e/CAC-TAT.spec.cy.js
@@ -8,4 +8,17 @@ describe("Central de Atendimento ao Cliente TAT", () => {
   it("verifica o título da aplicação", () => {
     cy.title().should("be.equal", "Central de Atendimento ao Cliente TAT");
   });
+
+  it("preenche os campos obrigatórios e envia o formulário", () => {
+    cy.get("#firstName").should("be.visible").type("Daniel");
+    cy.get("#lastName").should("be.visible").type("Silva");
+    cy.get("#email").should("be.visible").type("dfsilva.dxp@gmail.com");
+    cy.get("#open-text-area")
+      .should("be.visible")
+      .type("Solicito o reset da senha de meu user dfsilva");
+
+    cy.get("button[type=submit]").click();
+
+    cy.get(".success").should("be.visible");
+  });
 });
