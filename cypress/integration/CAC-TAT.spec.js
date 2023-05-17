f8fd7bb45de37760b108435beade99786a2486db
Walid Arnous <walidarnous.1@gmail.com>
2023-05-17 19:48:54 -0300

    Exercicio 4

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -9,7 +9,8 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     it('verifica o título da aplicação', function() {
         cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
     })
-
+    
+    //#1
     it('preenche os campos obrigatórios e envia o formulário', function(){
         const longText = 'Teste Teste, Teste Teste Teste Teste, Teste Teste Teste Teste, Teste Teste Teste Teste, Teste Teste Teste Teste, Teste Teste Teste Teste, Teste Teste Teste Teste, Teste Teste Teste Teste, Teste Teste Teste Teste, Teste Teste Teste Teste, Teste Teste Teste Teste'
         cy.get('#firstName').type('Walid')
@@ -22,7 +23,8 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('.success').should('be.visible')
     })
 
-    it.only('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function(){
+    //#2
+    it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function(){
         cy.get('#firstName').type('Walid')
         cy.get('#lastName').type('Arnous')
         cy.get('#email').type('walid...teste.com.br')
@@ -31,5 +33,26 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
         cy.get('.error').should('be.visible')
     })
+    
+    //#3
+    it('campo telefone continua vazio quando preenchido com valor não-numérico', function(){
+        cy.get('#phone')
+            .type('abcdefghij')
+            .should('have.value', '')
+    })
+
+    //#4
+    it.only('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function(){
+        cy.get('#firstName').type('Walid')
+        cy.get('#lastName').type('Arnous')
+        cy.get('#email').type('walid@teste.com.br')
+        cy.get('#open-text-area').type('Teste')
+        cy.get('#phone-checkbox').click()
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.error').should('be.visible')
+    })
+
+
 })
   
