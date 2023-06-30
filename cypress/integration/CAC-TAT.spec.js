46d8c1bf1d73afb2e41d74bd1bc776a513bd81f8
Andrea Gonzalez <andreavgonzalez7@gmail.com>
2023-06-30 15:34:40 -0300

    resolucao exercicio 1

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -14,6 +14,15 @@ describe('Central de Atendimento ao Cliente TAT', function() {
   })
     it('verifica o título da aplicação', function() {
       cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
-  
+    })
+    it('preenche os campos obrigatórios e envia o formulário', () => {
+      cy.get('#firstName').type('Ana')
+      cy.get('#lastName').type('Silva')
+      cy.get('#email').type('ana@email.com')
+      cy.get('#phone').type('55999899658')
+      cy.get('#open-text-area').type('Testes com cypress curso Walmyr')
+      cy.get('.button[type="submit"]').click({forece: true})
+
+      cy.get('.success').should('be.visible')
     })
   })
