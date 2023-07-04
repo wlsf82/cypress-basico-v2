0330b7ed45e3d2278bc0101aa55084337befe14d
Andrea Gonzalez <andreavgonzalez7@gmail.com>
2023-07-04 10:57:05 -0300

    utilizando comandos personalizados

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -32,18 +32,15 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
       cy.get('#firstName').type('Ana')
       cy.get('#lastName').type('Silva')
-      cy.get('#email').type('ana@email.com')
-      cy.get('#phone').type('55999899658')
       cy.get('#email').type('ana@.com')
+      cy.get('#phone').type('55999899658')
       cy.get('.button[type="submit"]').click({forece: true})
       
       cy.get('.error').should('be.visible')
     })
     //exercio extra 3
     it('verifica se o campo telefone contém apenas valores numéricos', () => {
-      cy.get('#phone')
-        .type('ana')
-        .should('have.value', '')     
+      cy.get('#phone').type('ana').should('have.value', '')     
     })
     //exercio extra 4
     it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
@@ -71,5 +68,19 @@ describe('Central de Atendimento ao Cliente TAT', function() {
       cy.get('.button[type="submit"]').click({forece: true})
       cy.get('.error').should('be.visible')
 
+    })
+    //exercicio extra 7 - comandos customizados
+    it('envia o formuário com sucesso usando um comando customizado', () => {
+      cy.fillMandatoryFieldsAndSubmit()
+
+      cy.get('.success').should('be.visible')
+    }) 
+    //exercício extra 8 - utilizar o cy.contains()
+    it('utilizar cy.contains para o botão enviar', () => {
+      cy.get('#firstName').type('Ana').should('have.value', 'Ana').clear().should('have.value', '')
+      cy.get('#lastName').type('Silva').should('have.value', 'Silva').clear().should('have.value', '')
+      cy.get('#email').type('ana@email.com').should('have.value', 'ana@email.com').clear().should('have.value', '')
+      cy.get('#open-text-area').type('Teste de digitação').should('have.value', 'Teste de digitação').clear().should('have.value', '')
+      cy.contains('button', 'Enviar').click({forece: true})
     })
   })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,9 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', () => {
+    cy.get('#firstName').type('Ana')
+    cy.get('#lastName').type('Silva')
+    cy.get('#email').type('ana@email.com')
+    cy.get('#phone').type('55999899658')
+    cy.get('#open-text-area').type('Teste digitação texto')
+    cy.get('.button[type="submit"]').click({forece: true})
+})
+
