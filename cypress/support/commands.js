5020978ccb2b3dfdffd9940d0a108340d485bc55
Thiago Mota <th.araujomota@gmail.com>
2023-07-21 14:11:27 -0300

    adiciona comando custom

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,7 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', () => {
+  cy.get('#firstName').type('John');
+  cy.get('#lastName').type('Doe');
+  cy.get('#email').type('john.doe@mail.com');
+  cy.get('#open-text-area').type('Teste');
+  cy.get('button[type="submit"').click();
+});
