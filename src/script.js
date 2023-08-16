e24f04ccb098cefeb51cdbc08c64a8871df19191
Rosane da Paixão <133041345+rosanepaixao@users.noreply.github.com>
2023-08-15 21:09:15 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -19,7 +19,7 @@ document.querySelector('#phone-checkbox')
 document.querySelector('button[type="submit"]')
   .addEventListener('click', function(event) {
     event.preventDefault()
-    const firstNameField = document.getElementById('TEstName')
+ 
     const firstNameField = document.getElementById('firstName')
     const lastNameField = document.getElementById('lastName')
     const emailField = document.getElementById('email')
