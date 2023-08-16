87710e4ca587c41194020cb9d7b8885061b9ceda
RicardoVolk <junior.volk@gmail.com>
2023-08-15 21:00:39 -0300

    Adicionando o custon commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,11 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', function() {
+    cy.get('#firstName').type('Walmyr')
+    cy.get('#lastName').type('Filho')
+    cy.get('#email').type('walmyr@exemplo.com')
+    cy.get('#open-text-area').type('Teste')
+    cy.contains('button', 'Enviar').click()
+  })
