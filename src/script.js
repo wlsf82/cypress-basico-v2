f255929659ce8d778908c58a7884d0b395d2eae3
ItamarPauolo <ips.ips@hotmail.com>
2023-08-22 07:25:20 -0300

    [Itamar Paulo]
    Ajuste de escrita

--- src/script.js
+++ src/script.js
@@ -36,7 +36,7 @@ document.querySelector('button[type="submit"]')
       return showAndHideErrorMessage()
     }
     if (!emailField.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
-      // return showAndHideErrorMessage()
+      return showAndHideErrorMessage()
     }
     firstNameField.value = ''
     lastNameField.value = ''
