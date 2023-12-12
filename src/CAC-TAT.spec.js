8252d0e7e34f5e6b3361812f90f155b1be5f2703
Ana Maria <amcastroesousa@gmail.com>
2023-12-12 08:44:07 -0300

    1teste

---
+++ src/CAC-TAT.spec.js
@@ -0,0 +1,10 @@
+/// <reference types="Cypress" />
+
+describe('Central de Atendimento ao Cliente TAT', function() {
+    it('verifica o título da aplicação', function() {
+        cy.visit('./src/index.html')
+
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+
+    })
+})
