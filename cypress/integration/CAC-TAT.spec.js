c84f8f133822f495bb420b6fb0fc8bfe08eb3ed6
Ana Maria <amcastroesousa@gmail.com>
2024-01-02 15:44:53 -0300

    config cypress

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -5,6 +5,5 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.visit('./src/index.html')
 
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
-
     })
 })
