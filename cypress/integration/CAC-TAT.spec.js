15a6a778d8750e04619b7e465eeb42b90d9d4bd0
Ana Maria <amcastroesousa@gmail.com>
2024-01-03 11:54:39 -0300

    Update CAC-TAT.spec.js

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -7,12 +7,13 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     it('verifica o título da aplicação', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
-    it('preenche os campos obrigatórios e envia o formulário', function() {
+    it('preenche os campos e envia o formulário', function() {
         cy.get('#firstName').type('Ana Maria')
         cy.get('#lastName').type('Teste da Silva Sauro')
         cy.get('#email').type('testezinhos@gmail.com')
         cy.get('#phone').type('44998219935')
         cy.get('#support-type > :nth-child(3)').click()
+        cy.get('#phone-checkbox').click()
         cy.get('#open-text-area').type('primeiro teste da ana')
         cy.get('button[type="submit"]').click()
 
