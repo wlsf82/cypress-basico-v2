48aefefedd55bcf53051903c525dbbd8b240ad9b
Ana Maria <amcastroesousa@gmail.com>
2024-01-03 10:21:49 -0300

    Primeiro teste completo

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,9 +1,20 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
+   beforeEach(function(){
+    cy.visit('./src/index.html')
+   })
     it('verifica o título da aplicação', function() {
-        cy.visit('./src/index.html')
-
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+    it('preenche os campos obrigatórios e envia o formulário', function() {
+        cy.get('#firstName').type('Ana Maria')
+        cy.get('#lastName').type('Teste da Silva Sauro')
+        cy.get('#email').type('testezinhos@gmail.com')
+        cy.get('#phone').type('44998219935')
+        cy.get('#support-type > :nth-child(3)').click()
+        cy.get('#open-text-area').type('primeiro teste da ana')
+        cy.get('button[type="submit"]').click()
+
     })
 })
