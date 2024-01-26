1b55b1956517e4bf6d0067507052287f9b8210a1
Bruno Correa Falanga <bruno.falanga@cwi.com.br>
2024-01-26 15:28:53 -0300

    Create cypress project

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -221,7 +221,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
       cy.get('#privacy a').should('have.attr','target','_blank')
     })
     
-    it.only('acessa a página da política de privacidade removendo o target e então clicando no link',()=>{
+    it('acessa a página da política de privacidade removendo o target e então clicando no link',()=>{
       cy.get('#privacy a')
         .invoke('removeAttr','target')
         .click()
