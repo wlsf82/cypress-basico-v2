4d26b32e93f7320d82915fc661bfd5745144aced
Rhaynner Assunção <rhaynner@rcaMac.local>
2024-01-30 11:48:58 -0300

    iniciando aula 3

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,26 +1,27 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', (name, lastName, email, longTest) => {
+    cy.get('#firstName')
+        .should('be.visible')
+        .type(name)
+        .should('have.value', name)
+
+    cy.get('#lastName')
+        .should('be.visible')
+        .type(lastName)
+        .should('have.value', lastName)
+
+    cy.get('#email')
+        .should('be.visible')
+        .type(email)
+        .should('have.value', email)
+
+    cy.get('#open-text-area')
+        .should('be.visible')
+        .type(longTest)
+        .should('have.value', longTest)
+
+    cy.get('.button')
+        .should('be.visible')
+        .click()
+
+})
