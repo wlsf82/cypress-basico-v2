af93e9f155c4c151a18af49cf59c50888cdbba0a
Fernanda <feh.cmrg@outlook.com>
2024-02-23 09:48:31 -0300

    feat: inicio de testes treinamento cypress basico juntamente com treinamento POM

---
+++ cypress/e2e/CAC-TAT.cy.js
@@ -0,0 +1,47 @@
+// Codigo fala para o visualstudio procurar referencias cypress 
+// para identificar parâmetros, methods e autocomplete
+
+const { CacTatObject } = require ('../pageobject/CAC-TAT-objects');
+
+/// <reference types="Cypress" /> 
+
+describe('TAT Customer Service Center', () => {
+
+    var cacTatObject;
+
+    beforeEach(() => {
+        cacTatObject = new CacTatObject();
+        cy.visit('../../src/index.html');
+    })
+
+    it('Check the apps title', function () {
+        cy.title()
+          .should('be.equal', 'Central de Atendimento ao Cliente TAT');
+    })
+
+    it('Fill in the required fields and submit the form', function () {
+        const text = 'Lorem ipsum dolor sit amet, consectetur';
+
+        cacTatObject.inputFirstName('Amy')
+                    .inputLastName('Lee')
+                    .inputEmail('amylee@gmail.com')
+                    .boxTextHowCanWeHelpYou(text)
+                    .buttonSend()
+                    .checkingMessageSuccess();
+    })
+
+    it('Fill in the required fields and submit the form with delay 0', function () {
+
+        const longText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,'+
+        'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet,'+
+        'consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'+
+        'consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+
+        cacTatObject.inputFirstName('Joe')
+                    .inputLastName('Ju')
+                    .inputEmail('jooujuue@gmail.com')
+                    .boxTextHowCanWeHelpYou(longText, 0)
+                    .buttonSend()
+                    .checkingMessageSuccess();
+    })
+})
---
+++ cypress/pageobject/CAC-TAT-objects.js
@@ -0,0 +1,41 @@
+
+/// <reference types="Cypress" /> 
+
+export class CacTatObject{
+
+  constructor(){}
+
+  inputFirstName(firstName) {
+    cy.get('#firstName').type(firstName);
+    return this
+  }
+
+  inputLastName(lastName) {
+    cy.get('#lastName').type(lastName);
+    return this
+  }
+
+  inputEmail(email) {
+    cy.get('#email').type(email);
+    return this;
+  }
+
+  boxTextHowCanWeHelpYou(textAskingForHelp, timeDelay = 10) {
+    const boxText = cy.get('#open-text-area');
+
+    timeDelay != 10 
+      ? boxText.type(textAskingForHelp, {  delay: timeDelay } ) 
+      : boxText.type(textAskingForHelp);
+    return this;
+  }
+
+  buttonSend(){
+    cy.get('button[type="submit"]').contains('Enviar').should('be.visible').click();
+    return this;
+  }
+
+  checkingMessageSuccess(){
+    cy.get('.success').should('be.visible');
+    return this;
+  }
+}
